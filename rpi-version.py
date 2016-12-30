110e2dea594a83b3e0d7233b3ec5d1de7a79ded6
Tigran Tch <nargitinthenight@gmail.com>
2016-12-31 00:41:36 +0100

    First commit

---
+++ rpi-version.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+import json
+import os
+
+with open('rpi.json') as json_file:
+    data = json.load(json_file)
+    revision = os.system("cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//'")
+    models = set()
+    for rpi in data["rpi"]:
+        if revision == rpi["revision"]:
+            print("version : {}".format(rpi["version"]))
+            print("   date : {}".format(rpi["release_date"]))
