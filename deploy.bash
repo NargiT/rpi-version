5a0a4bf9768546e00348785389171a46944d9579
Tigran Tch <nargitinthenight@gmail.com>
2019-03-22 00:56:59 +0100

    add script

---
+++ deploy.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    TAG=$(grep "ENV RPI_VERSION" Dockerfile | awk 'NF>1{print $NF}')
+    docker tag nargit/rpi-version nargit/rpi-version:$TAG
+    docker push nargit/rpi-version:$TAG
+    docker push nargit/rpi-version
+fi
