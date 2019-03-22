70216f89b732da79534aed6e14996a195ffeaf76
Tigran Tch <nargitinthenight@gmail.com>
2019-03-22 01:11:06 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ $TRAVIS_PULL_REQUEST == "false" ]; then
+if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
     docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
     TAG=$(grep "ENV RPI_VERSION" Dockerfile | awk 'NF>1{print $NF}')
     docker tag "nargit/rpi-version nargit/rpi-version:$TAG"
